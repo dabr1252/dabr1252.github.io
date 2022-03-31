830c88c85040bca9a6aa68509f57719322644780
dabr1252 <89607461+dabr1252@users.noreply.github.com>
2022-03-31 10:14:52 -0600

    changes to home page

---
+++ javascript/assignment11.js
@@ -0,0 +1,9 @@
+document.querySelector('h1').addEventListener('click', function() {
+  alert('DANIELLA BRUZZONE');
+});
+
+document.getElementById("myBtn").addEventListener("click", visitPage);
+
+function visitPage(){
+        window.location='https://www.linkedin.com/in/daniella-bruzzone-95b5681a8/';
+    }
